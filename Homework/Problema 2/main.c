93a43f0d7b8b4e5fba7b56b6ef7e396ff208cca0
Raffaele <raffio98@gmail.com>
2022-10-22 00:46:35 +0200

    test generation aggiunta

--- Homework/Problema 2/main.c
+++ Homework/Problema 2/main.c
@@ -1,18 +1,54 @@
 #include "utils.c"
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+
+// Number of tests to execute defined here
+#define NUM_TESTS 3
 
 int main(){
 
-    int vet[] = {5,5,5,5,5,5,5,5};
-    
-    int *b = algoritmo_v1(vet, sizeof(vet)/sizeof(vet[0]));
+    // Test genetarion
+    int *tests[NUM_TESTS];
+    int sizes[NUM_TESTS];
 
-    for (size_t i = 0; i < sizeof(vet)/sizeof(vet[0]); i++)
-    {
-        printf("%d ", b[i]);
+    srand(time(NULL));
+
+    for (int i = 0; i < NUM_TESTS; i++){
+        int size = (rand()%10) + 1;
+        sizes[i] = size;
+        tests[i] = malloc(sizeof(int)*size);
+
+        for (int k = 0; k < size; k++)
+            tests[i][k] = rand()%20;
+        
     }
-    printf("\n");
-    
-    
+    // End of test generation
+
+    int *results[NUM_TESTS];
+
+    // Execute algorithm
+    for (int i = 0; i < NUM_TESTS; i++)
+        results[i] = algoritmo_v1(tests[i],sizes[i]);
+
+    // Print of the input and results arrays
+    for (int i = 0; i < NUM_TESTS; i++){
+
+        printf("Stampo l'array di input numero %d e sotto il relativo risultato dell'esecuzione:\n",i+1);
+        for (int k = 0; k < sizes[i]; k++)
+            printf("%d ",tests[i][k]);
+        printf("\n");
+        for (int k = 0; k < sizes[i]; k++)
+            printf("%d ",results[i][k]);
+        printf("\n");
+        printf("----------");
+        printf("\n");
+
+    }
+
+    for (int i = 0; i < NUM_TESTS; i++){
+        free(tests[i]);
+        free(results[i]);
+    }
+
 }
