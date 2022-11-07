162d4e92f499781928da85c01d291ce0522746a5
Raffaele <raffio98@gmail.com>
2022-11-07 23:26:05 +0100

    es 3 31 ottobre terminato

--- Esercitazione 3 - 31 Ottobre/3/main.c
+++ Esercitazione 3 - 31 Ottobre/3/main.c
@@ -6,10 +6,13 @@
 
 #define MAX_PANCAKES 30
 
+#define DIM 5
+
 void print_array(int *A, int len);
 void flip(int *pancake_vector, int p_vector_size, int flip_index);
 int pancake_flips(int *pancakes_vector, int p_vector_size, int *solution_vector);
 int find_index_max(int *A, int start, int end);
+
 
 int main(){
 
@@ -24,22 +27,24 @@ int main(){
     while (c = getc(f)){
         
         if (c == '\n'){
-
+            
+            print_array(pancakes,index);
+            printf("\n");
             solution_size = pancake_flips(pancakes,index,solution);
             print_array(solution,solution_size);
-            printf("\n");
+            printf("\n\n");
             index = 0;
             solution_size = 0;
         }
-        else if (c == EOF && index != 0)
-        {
+        else if (c == EOF && index != 0){
 
+            print_array(pancakes,index);
+            printf("\n");
             solution_size = pancake_flips(pancakes,index,solution);
             print_array(solution,solution_size);
-            printf("\n");
+            printf("\n\n");
             break;
         }
-        
         
         while (c == ' ')
             c = getc(f);
@@ -72,22 +77,26 @@ int find_index_max(int *A, int start, int end){
 
 void flip(int *pancake_vector, int p_vector_size, int flip_index){
 
-    if (flip_index == 0)
+    if (flip_index <= 0 || (p_vector_size-flip_index) < 1){
+        printf("Esco senza eseguire\n");
         return;
+    }
 
     int adj_flip_index = p_vector_size - flip_index;
     
-    if (adj_flip_index % 2 == 0)
-        adj_flip_index = adj_flip_index/2;
-    else
-        adj_flip_index = adj_flip_index/2 + 1;
-    
+    int one = 0;
+    if (adj_flip_index % 2 != 0)
+        one = 1;
+
+    adj_flip_index = adj_flip_index/2;
+
     int temp;
     for (int i = 0; i <= adj_flip_index; i++){
 
         temp = pancake_vector[i];
-        pancake_vector[i] = pancake_vector[adj_flip_index-i];
-        pancake_vector[adj_flip_index-i] = temp;
+
+        pancake_vector[i] = pancake_vector[2*adj_flip_index-i + one];
+        pancake_vector[2*adj_flip_index-i + one] = temp;
     }
     
 }
@@ -99,19 +108,26 @@ int pancake_flips(int *pancakes_vector, int p_vector_size, int *solution_vector)
 
     for (int i = 0; i < p_vector_size; i++){
 
-        max_pos = find_index_max(pancakes_vector,0,p_vector_size-i);
-        if (max_pos != p_vector_size-i-1){
+        max_pos = find_index_max(pancakes_vector,0,p_vector_size-i-1);
 
-            //Qui effettuo i flip
-            flip(pancakes_vector,p_vector_size,max_pos+1);
-            solution_vector[s_vector_size] = max_pos+1;
+        if (max_pos != p_vector_size-1-i)
+        {
+            if (max_pos != 0){
+                flip(pancakes_vector,p_vector_size,p_vector_size - max_pos);
+                solution_vector[s_vector_size] = p_vector_size - max_pos;
+                s_vector_size++;
+            }
+
+            flip(pancakes_vector,p_vector_size,i+1);
+            solution_vector[s_vector_size] = i+1;
             s_vector_size++;
-            flip(pancakes_vector,p_vector_size,1);
-            solution_vector[s_vector_size] = 1;
-            s_vector_size++;
+            
         }
         
     }
+    
+    solution_vector[s_vector_size] = 0;
+    s_vector_size++;
 
     return s_vector_size;
     
