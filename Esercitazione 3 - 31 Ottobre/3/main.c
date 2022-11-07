4ddc00ee29b23c44754ef7ce4b313434b35e1b5d
Raffaele <raffio98@gmail.com>
2022-11-07 13:18:53 +0100

    es 3 31 ott

--- Esercitazione 3 - 31 Ottobre/3/main.c
+++ Esercitazione 3 - 31 Ottobre/3/main.c
@@ -4,34 +4,39 @@
 #include <assert.h>
 #include "ll.c"
 
-#define c2i(c) (c - '0')
+#define MAX_PANCAKES 30
 
-int *pancake_flips(int *A, int size);
+void print_array(int *A, int len);
+void flip(int *pancake_vector, int p_vector_size, int flip_index);
+int pancake_flips(int *pancakes_vector, int p_vector_size, int *solution_vector);
+int find_index_max(int *A, int start, int end);
 
 int main(){
-
-    LinkedList *results = createEmptyList();
 
     FILE *f = fopen("test.txt","r");
     assert(f != NULL);
 
     int c;
     int index = 0;
-    int pancakes[30] = {0};
+    int pancakes[MAX_PANCAKES] = {0};
+    int solution[2*MAX_PANCAKES] = {0};
+    int solution_size;
     while (c = getc(f)){
         
         if (c == '\n'){
 
-            int *solution = pancake_flips(pancakes,index);
-            llNode *result = createNode(solution);
-            append(results,result);
+            solution_size = pancake_flips(pancakes,index,solution);
+            print_array(solution,solution_size);
+            printf("\n");
             index = 0;
+            solution_size = 0;
         }
         else if (c == EOF && index != 0)
         {
-            int *solution = pancake_flips(pancakes,index);
-            llNode *result = createNode(solution);
-            append(results,result);
+
+            solution_size = pancake_flips(pancakes,index,solution);
+            print_array(solution,solution_size);
+            printf("\n");
             break;
         }
         
@@ -50,13 +55,71 @@ int main(){
     
 }
 
-int *pancake_flips(int *A, int size){
-    //test: stampo l'array
-    for (size_t i = 0; i < size; i++)
-    {
-        printf("%d ",A[i]);
-    }
-    printf("\n");
+/*
+* Returns the index of the max elem of [A] from the [start] to the [end] index included
+*/
+int find_index_max(int *A, int start, int end){
     
-    return NULL;
+    int index_max = start;
+
+    for (int i = start+1; i <= end; i++)
+        if (A[i]> A[index_max])
+            index_max = i;
+    
+    return index_max;
+    
+}
+
+void flip(int *pancake_vector, int p_vector_size, int flip_index){
+
+    if (flip_index == 0)
+        return;
+
+    int adj_flip_index = p_vector_size - flip_index;
+    
+    if (adj_flip_index % 2 == 0)
+        adj_flip_index = adj_flip_index/2;
+    else
+        adj_flip_index = adj_flip_index/2 + 1;
+    
+    int temp;
+    for (int i = 0; i <= adj_flip_index; i++){
+
+        temp = pancake_vector[i];
+        pancake_vector[i] = pancake_vector[adj_flip_index-i];
+        pancake_vector[adj_flip_index-i] = temp;
+    }
+    
+}
+
+int pancake_flips(int *pancakes_vector, int p_vector_size, int *solution_vector){
+    
+    int s_vector_size = 0;
+    int max_pos;
+
+    for (int i = 0; i < p_vector_size; i++){
+
+        max_pos = find_index_max(pancakes_vector,0,p_vector_size-i);
+        if (max_pos != p_vector_size-i-1){
+
+            //Qui effettuo i flip
+            flip(pancakes_vector,p_vector_size,max_pos+1);
+            solution_vector[s_vector_size] = max_pos+1;
+            s_vector_size++;
+            flip(pancakes_vector,p_vector_size,1);
+            solution_vector[s_vector_size] = 1;
+            s_vector_size++;
+        }
+        
+    }
+
+    return s_vector_size;
+    
+}
+
+void print_array(int *A, int len){
+    printf("[");
+    for(int i=0;i<len;i++)
+        printf("%d ", A[i]);
+    printf("]");
 }
