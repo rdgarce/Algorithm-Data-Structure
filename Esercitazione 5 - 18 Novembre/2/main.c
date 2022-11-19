ab1c88d5316eeb0e9826993fdf7ba0220305b97c
Raffaele <raffio98@gmail.com>
2022-11-19 20:03:42 +0100

    updates

--- Esercitazione 5 - 18 Novembre/2/main.c
+++ Esercitazione 5 - 18 Novembre/2/main.c
@@ -5,14 +5,25 @@
 
 // Buon a parte qualche piccola miglioria di carattere costante sul tempo asintotico.
 
+#define MAX_SIZE 3000
 bool is_jolly(int *sequence, int s_size);
 
 int main(){
 
-    int a[] = {1, 4, 2, -1, 6};
-    int res = is_jolly(a,5);
+    int array[MAX_SIZE];
+    char t_buff;
+    int a_size = 0;
 
-    printf("%d",res);
+    while ((t_buff = getc(stdin)) != EOF && t_buff != '\n')
+    {
+        ungetc(t_buff,stdin);
+        scanf("%d",&a_size);
+        for (int i = 0; i < a_size; i++)
+        {
+            scanf("%d",&array[i]);
+        }
+        is_jolly(array,a_size) ? printf("Jolly\n") : printf("Not jolly\n");
+    }
 
 }
 
